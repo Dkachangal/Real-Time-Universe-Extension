f09a951103d6c6183107015b920ecfc7cad41a0b
Divyansh Kachangal <dkachangal@gmail.com>
2026-07-17 15:06:46 +0530

    react vite setup

---
+++ extenssion/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="theme-container">
+      <div className="dashboard">
+        <h1>My Future Theme Dashboard</h1>
+        <p>Testing locally before making it an extension!</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
