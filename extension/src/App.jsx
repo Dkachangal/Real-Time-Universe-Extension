a1b7ab2e717753259141dac51b66fb16686546c7
Divyansh Kachangal <dkachangal@gmail.com>
2026-07-17 15:22:06 +0530

    setting up react three fiber

---
+++ extension/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Texture } from 'three';
+import ThreeScene from '../components/index'
+
+const App = () => {
+  return (
+    <div className="theme-container">
+      <ThreeScene />
+    </div>
+  );
+};
+
+export default App;
